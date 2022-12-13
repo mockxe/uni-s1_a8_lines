bf8ecf59e15b9fb09654a0c1b82bcfefd22c1408
mockxe <38732499+mockxe@users.noreply.github.com>
2022-12-13 19:09:38 +0100

    Initial commit 🫐

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "io.mockxe.uni.s1.a8.lines"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "s1_a8_lines"
+
